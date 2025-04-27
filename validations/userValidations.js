28d65fa0c3fa6cb60bfdba817c0989065447d5a1
Roshan Bhusal <128559779+robh-01@users.noreply.github.com>
2025-04-28 00:59:28 +0545

    Add sanitization to the data

--- validations/userValidations.js
+++ validations/userValidations.js
@@ -2,20 +2,26 @@ import { body } from "express-validator";
 
 const validateName = [
   body("name")
+    .trim()
+    .escape()
     .notEmpty()
     .withMessage("Name is required.")
-    .isLength({ min: 2 })
-    .withMessage("Name must be at least 2 characters long.")
+    .isLength({ min: 2, max: 50 })
+    .withMessage("Name must be between 2 and 50 characters long.")
     .matches(/^[a-zA-Z\s]+$/)
     .withMessage("Name must contain only letters and spaces."),
 ];
 
 const validatePassword = [
   body("password")
+    .trim()
+    .escape()
     .notEmpty()
     .withMessage("Password is required.")
     .isLength({ min: 8 })
     .withMessage("Password must be at least 8 characters long.")
+    .isLength({ max: 20 })
+    .withMessage("Password must be at most 20 characters long.")
     .matches(/^(?=.*[a-z])/) // at least one lowercase
     .withMessage("Password must include at least one lowercase letter")
     .matches(/^(?=.*[A-Z])/) // at least one uppercase
@@ -30,8 +36,12 @@ const validatePassword = [
 
 const validateEmail = [
   body("email")
+    .trim()
+    .escape()
     .notEmpty()
     .withMessage("Email is required.")
+    .isLength({ max: 100 })
+    .withMessage("Email must be at most 100 characters long.")
     .isEmail()
     .withMessage("Invalid email format.")
     .normalizeEmail(), // Sanitize email by removing unnecessary characters and standardizing format
